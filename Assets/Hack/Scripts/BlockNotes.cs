8b7eb4fcd3c11723a6db054d868766bee36d3e68
Ryan Sullivan <RyanSullivan@gmail.com>
2019-01-19 23:44:07 -0500

    Started blocking

---
+++ Assets/Hack/Scripts/BlockNotes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockNotes : MonoBehaviour
+{
+
+    private string[] notes = { "I love you", "I miss you so much", "I wish you were here", "It was hard to see you suffer", "It was too soon", "We were meant to grow old together" };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
